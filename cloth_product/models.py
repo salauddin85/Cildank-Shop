3565bde7be80476b4e74a9fe8d1b2d732b0a0f09
salauddin85 <ahmedsalauddin677785@gmail.com>
2024-08-27 23:44:50 +0600

    cloudinary  add

--- cloth_product/models.py
+++ cloth_product/models.py
@@ -4,13 +4,13 @@ from cloth_category.models import Category,Sub_Category
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from .constraints import SIZE,STAR_CHOICES
-
+from cloudinary.models import CloudinaryField
 class Product(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     sub_category = models.ForeignKey(Sub_Category,on_delete=models.CASCADE,null=True,blank=True)
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='Product_images')
+    image = CloudinaryField('Product_images')
     price = models.DecimalField(decimal_places=2,max_digits=10)
     quantity = models.IntegerField()
     description = models.TextField()
